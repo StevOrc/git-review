aefd41135e84ba431f6b82859f763b46d8f6857b
Steven Orcun <steven.orcun@fintecture.com>
2022-07-01 15:51:21 +0200

    feat: GR-001 Add config for eslint n the project

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { errorHandling } from "./middlewares/error-hanlder.js";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { errorHandling } from './middlewares/error-hanlder.js';
 
 dotenv.config();
 const PORT = process.env.NODE_PORT || 3000;
@@ -11,14 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("*", () => {
-  throw new Error("Route not found...");
+app.use('*', () => {
+    throw new Error('Route not found...');
 });
 
 app.use(errorHandling);
 
 app.listen(PORT, () => {
-  console.log(`Server listenig on port::${PORT}`);
+    console.log(`Server listenig on port::${PORT}`);
 });
 
 export default app;
--- middlewares/error-hanlder.js
+++ middlewares/error-hanlder.js
@@ -1,5 +1,8 @@
 export function errorHandling(err, req, res, next) {
-  if (err) {
-    res.status(400).send({ message: err.message });
-  }
+    if (err) {
+        console.log(req.status);
+        res.status(400).send({ message: err.message });
+    }
+
+    next();
 }
